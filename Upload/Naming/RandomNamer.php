ab655c963b52545bbd158724267529db48173f10
Dennis Senn <d.senn@webintex.ch>
2016-03-07 13:27:03 +0100

    added needed(?) getRandomFileName method to namer

--- Upload/Naming/RandomNamer.php
+++ Upload/Naming/RandomNamer.php
@@ -18,7 +18,10 @@ class RandomNamer implements NamerInterface
 		/* @var UploadedFile $file */
 		$file = $mapping->getFile($entity);
 		$file_name = $file->getClientOriginalName();
-		$extension = substr($file_name, strrpos($file_name, '.') + 1);
+
+		return $this->getRandomFileName($file_name, $mapping->getUploadDestination());
+
+		/*$extension = substr($file_name, strrpos($file_name, '.') + 1);
 
 		// create random_string
 		$random_string = time() . '_';
@@ -27,6 +30,23 @@ class RandomNamer implements NamerInterface
 		// check if file exists on upload_path
 		if (is_file($mapping->getUploadDestination() . '/' . $random_string)) {
 			return $this->name($entity, $mapping);
+		}
+
+		return $random_string . '.' . $extension;*/
+	}
+
+	public function getRandomFileName($file_name, $directory)
+	{
+		// get extension
+		//$extension = preg_replace('/\.([\w]+)$/', '$1', $file_name);
+		$extension = substr($file_name, strrpos($file_name, '.') + 1);
+
+		// create random_string
+		$random_string = time() . '_';
+		$random_string .= substr(str_shuffle("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVW0123456789"), 0, 20);
+
+		if (is_file($directory . '/' . $random_string . '.' . $extension)) {
+			return $this->getRandomFileName($file_name, $directory);
 		}
 
 		return $random_string . '.' . $extension;
